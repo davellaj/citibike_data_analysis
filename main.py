aee7dc9bd669f95c1face43fb210882572e588fa
davellaj <jamie.davella@gmail.com>
2017-01-26 00:03:35 -0500

    working program to compute average distance from metro using data sets

--- main.py
+++ main.py
@@ -11,87 +11,17 @@
 # site where you can view the queries I made, or maybe a blog post that allows users to post suggestions for queries.
 # place where people can post their work/queries/projects/ideas about public transportation
 
-from math import radians, cos, sin, asin, sqrt
-import csv
-# import requests
-
-# request object gives us access to json method
-# r = requests.get("URL")
-# data = r.json()
-
-# import urllib2
-# import json
-
-# citibikesAPI = 'AIzaSyAVSDb3H4ZXQ7i8fWhDO9AOPBNmNLhL6x4'
-#
-# def locu_search(query):
-#     api_key = citibikesAPI
-#     url = 'https://api.locu.com/v1_0/venue/search/?api_key' + api_key
-#     locality = query.replace(' ', '%20')
-#     final_url = url + '&locality=' + locality + '&category=restaurant'
-#     json_obj = urllib2.urlopen(final_url)
-#     data = json.load(json_obj)
-#
-#     for item in data['objects']:
-#         print item['name']
-#         print item['phone']
-#
-# locu_search('new york')
-
-
-# haversine is spherical string pull tight from one point to another. appropriate for drone delivery :)
-# triangular equation - pythagoros point to point on 2D field
-#  Manhattan Distance -- diff in one coordinate and diff in another coordinate and adding them
-#  you only walk along the grid == assumes you was walking across the grid
-
-# links from chris
-# r = requests.get(".........")
-# data = r.json()
-# pip install requests
-# import requests
-# import psycopg2
-# http://www.citibikenyc.com/stations/json
-# https://www.citibikenyc.com/system-data
-# Manhattan Distance
-# import csv
-# https://docs.python.org/3/library/csv.html
-
-
-# def haversine(lon1, lat1, lon2, lat2):
-#     """
-#     Calculate the great circle distance between two points
-#     on the earth (specified in decimal degrees)
-#     """
-#     # convert decimal degrees to radians
-#     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-#     # haversine formula
-#     dlon = lon2 - lon1
-#     dlat = lat2 - lat1
-#     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-#     c = 2 * asin(sqrt(a))
-#     km = 6367 * c
-#     return km
-
-def manhattan_distance(start, end):
-    s_lat, s_lon = start
-    e_lat, e_lon = end
-    # print(start, end)
-    return abs(e_lat - s_lat) + abs(e_lon - s_lon)
-
-# search through data set and look for closest latlong to citibike station
-# calculate latlong distance:
-
 # example input
 # metro_stations = [
 #                     {'lat': 40.730328, 'lon': -73.992629, 'name': '8th St'},
 #                     {'lat': 40.741303, 'lon': -73.989344, 'name': '23rd St'},
 #                     {'lat': 40.735736, 'lon': -73.990568, 'name': 'Union Square'},
 #                 ]
-citibike_stations = [
-                        {'lat': 40.7423543, 'lon': -73.98915076, 'name': 'Broadway & W 24 St' },
-                        {'lat': 40.751551, 'lon': -73.993934, 'name': '8 Ave & W 33 St' },
-                        {'lat': 40.746745, 'lon': -74.007756, 'name': 'W 20 St & 11 Ave' },
-                    ]
+# citibike_stations = [
+#                         {'lat': 40.7423543, 'lon': -73.98915076, 'name': 'Broadway & W 24 St' },
+#                         {'lat': 40.751551, 'lon': -73.993934, 'name': '8 Ave & W 33 St' },
+#                         {'lat': 40.746745, 'lon': -74.007756, 'name': 'W 20 St & 11 Ave' },
+#                     ]
 
 # example output
 # [
@@ -115,7 +45,38 @@ citibike_stations = [
 # ]
 # observation - top citibike station all next to the same metro stop
 
-# getting data from metro station csv file
+
+import csv
+import requests
+
+# request object gives us access to json method
+r = requests.get("https://feeds.citibikenyc.com/stations/stations.json")
+data = r.json()
+
+
+
+
+# links from chris
+# r = requests.get(".........")
+# data = r.json()
+# pip install requests
+# import requests
+# import psycopg2
+# http://www.citibikenyc.com/stations/json
+# https://www.citibikenyc.com/system-data
+
+
+# manhattan distance is a formula for computing distance in right angles,
+# assuming you can only walk north south east west on a street grid like manhattan
+def manhattan_distance(start, end):
+    s_lat, s_lon = start
+    e_lat, e_lon = end
+    # print(start, end)
+    return abs(e_lat - s_lat) + abs(e_lon - s_lon)
+
+
+# getting data from metro station csv file: https://docs.python.org/3/library/csv.html
+# would like to clean up and remove duplicate station locations to reduce loop runtime
 # Division,Line,Station_Name,Station_Latitude,Station_Longitude,Route_1,Route_2,Route_3,Route_4,Route_5,Route_6,Route_7,Route_8,Route_9,Route_10,Route_11,Entrance_Type,Entry,Exit_Only,Vending,Staffing,Staff_Hours,ADA,ADA_Notes,Free_Crossover,North_South_Street,East_West_Street,Corner,Latitude,Longitude
 metro_stations_csv = []
 with open('metro_station_entrances_nyc.csv') as csvfile:
@@ -129,6 +90,7 @@ with open('metro_station_entrances_nyc.csv') as csvfile:
                                 })
 #  citibike best station dataset
 # only selected usertype to be subscribers as opposed to customers
+# would like to clean up and remove the bottom 5
 citibike_best_stations_csv = []
 with open('citibike_best_desc.csv') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -142,6 +104,7 @@ with open('citibike_best_desc.csv') as csvfile:
                                         })
 # citibike worst stations data set
 # only selected usertype to be subscribers as opposed to customers
+# would like to clean up and remove bottom 5
 citibike_worst_stations_csv = []
 with open('citibike_worst_asc.csv') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -157,10 +120,11 @@ with open('citibike_worst_asc.csv') as csvfile:
 # print(citibike_best_stations_csv)
 
 
-def get_distance():
-    mode_distance = []
-    for citibike in citibike_stations_csv:
-        distance_to_metro = 1000
+def get_distance_citibike_best():
+    citibike_best_distance = []
+
+    for citibike in citibike_best_stations_csv:
+        distance_to_metro = 10000
         shortest_metro = {}
         for metro in metro_stations_csv:
             temp_distance = manhattan_distance((metro['lat'], metro['lon']), (citibike['lat'], citibike['lon']))
@@ -169,8 +133,92 @@ def get_distance():
             if temp_distance < distance_to_metro:
                 distance_to_metro = temp_distance
                 shortest_metro = metro
-        mode_distance.append({'citibike_station': citibike, 'metro_station': shortest_metro, 'distance_to_metro': distance_to_metro})
-    return mode_distance
+        citibike_best_distance.append({'citibike_station': citibike, 'metro_station': shortest_metro, 'distance_to_metro': distance_to_metro})
+    return citibike_best_distance
 
-# get_distance()
-# print(get_distance())
+def get_distance_citibike_worst():
+    citibike_worst_distance = []
+
+    for citibike in citibike_worst_stations_csv:
+        distance_to_metro = 10000
+        shortest_metro = {}
+        for metro in metro_stations_csv:
+            temp_distance = manhattan_distance((metro['lat'], metro['lon']), (citibike['lat'], citibike['lon']))
+            # temp_distance = haversine(metro['lon'], metro['lat'], citibike['lon'], citibike['lat'])
+
+            if temp_distance < distance_to_metro:
+                distance_to_metro = temp_distance
+                shortest_metro = metro
+        citibike_worst_distance.append({'citibike_station': citibike, 'metro_station': shortest_metro, 'distance_to_metro': distance_to_metro})
+    return citibike_worst_distance
+
+# print(get_distance_citibike_best())
+# print(get_distance_citibike_worst())
+
+# look at distance from best stations and look with distance from worst stations
+# loop over these objects to print out distance.
+# do the distances have a trend, avg distance?
+def print_distances():
+    best_distances = get_distance_citibike_best()
+    worst_distances = get_distance_citibike_worst()
+    best_avg = 0
+    best_count = 0
+    worst_avg = 0
+    worst_count = 0
+
+    print("Best station distances")
+    for station in best_distances:
+        best_avg += station['distance_to_metro']
+        best_count += 1
+        print("Distance to metro: ", station['distance_to_metro'], "Citibike Station: ", station['citibike_station']['citibike_station_name'] )
+
+    print("Worst station distances")
+    for station in worst_distances:
+        worst_avg += station['distance_to_metro']
+        worst_count += 1
+        print("Distance to metro: ", station['distance_to_metro'], "Citibike Station: ", station['citibike_station']['citibike_station_name'] )
+    print("Best Citibike station AVG distance to Metro: ", best_avg/best_count)
+    print("Worst Citibike station AVG distance to Metro: ", worst_avg/worst_count)
+
+print_distances()
+
+
+# Notes----------------------------------------------------------------------------------------
+# from math import radians, cos, sin, asin, sqrt
+# haversine is spherical string pull tight from one point to another. appropriate for drone delivery :)
+# triangular equation - pythagoros point to point on 2D field
+#  Manhattan Distance -- diff in one coordinate and diff in another coordinate and adding them
+#  you only walk along the grid == assumes you was walking across the grid
+
+# def haversine(lon1, lat1, lon2, lat2):
+#     """
+#     Calculate the great circle distance between two points
+#     on the earth (specified in decimal degrees)
+#     """
+#     # convert decimal degrees to radians
+#     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+#     # haversine formula
+#     dlon = lon2 - lon1
+#     dlat = lat2 - lat1
+#     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+#     c = 2 * asin(sqrt(a))
+#     km = 6367 * c
+#     return km
+#  ------------------------
+# youtube tutorial on using an api with python - in python2
+# import urllib2
+# import json
+#
+# def locu_search(query):
+#     api_key = ''
+#     url = 'https://api.locu.com/v1_0/venue/search/?api_key' + api_key
+#     locality = query.replace(' ', '%20')
+#     final_url = url + '&locality=' + locality + '&category=restaurant'
+#     json_obj = urllib2.urlopen(final_url)
+#     data = json.load(json_obj)
+#
+#     for item in data['objects']:
+#         print item['name']
+#         print item['phone']
+#
+# locu_search('new york')
