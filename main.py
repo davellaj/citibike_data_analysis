1fa910455db61cfa793cd32fe6cddb66c1560ee8
davellaj <jamie.davella@gmail.com>
2017-01-25 19:53:19 -0500

    put csv file into dictionary and changed distance function to be manhattan distance

--- main.py
+++ main.py
@@ -6,7 +6,11 @@
 # return the distance to metro station for the top 10 stations
 # return the distance to metro station for the bottom 10 stations
 # print results on scatterplot with circles to reflect distance or a bar chart one mar is metro stop one bar is distance
+# make interface where my friends who aren't tech savy can draw information for the queries
+# site where you can view the queries I made, or maybe a blog post that allows users to post suggestions for queries.
+
 from math import radians, cos, sin, asin, sqrt
+import csv
 # import requests
 
 # request object gives us access to json method
@@ -32,14 +36,7 @@ from math import radians, cos, sin, asin, sqrt
 #
 # locu_search('new york')
 
-# calculate latlong distance:
-citibike_station = {'lat': 40.7423543, 'lon': -73.98915076, 'name': 'Broadway & W 24 St' }
-metro_station = {'lat': 40.730328, 'lon': -73.992629, 'name': 'Broadway, 8th St'}
-metro_stations = [
-                    {'lat': 40.730328, 'lon': -73.992629, 'name': 'Broadway, 8th St'},
-                    {'lat': 40.741303, 'lon': -73.989344, 'name': 'Broadway,23rd St'},
-                    {'lat': 40.735736, 'lon': -73.990568, 'name': 'Broadway,Union Square'},
-                ]
+
 # haversine is spherical string pull tight from one point to another. appropriate for drone delivery :)
 # triangular equation - pythagoros point to point on 2D field
 #  Manhattan Distance -- diff in one coordinate and diff in another coordinate and adding them
@@ -57,6 +54,7 @@ metro_stations = [
 # import csv
 # https://docs.python.org/3/library/csv.html
 
+
 def haversine(lon1, lat1, lon2, lat2):
     """
     Calculate the great circle distance between two points
@@ -72,13 +70,76 @@ def haversine(lon1, lat1, lon2, lat2):
     km = 6367 * c
     return km
 
-# search through data set and look for closest latlong
-distance = 1000
-for item in metro_stations:
-    # print(item['lat'])
-    temp_distance = haversine(item['lon'], item['lat'], citibike_station['lon'], citibike_station['lat'])
-    if temp_distance < distance:
-        distance = temp_distance
-print(distance)
+def manhattan_distance(start, end):
+    s_lat, s_lon = start
+    e_lat, e_lon = end
+    # print(start, end)
+    return abs(e_lat - s_lat) + abs(e_lon - s_lon)
 
-# print("the answer in km should be 1.369: ", haversine(citibike_station['lon'], citibike_station['lat'], metro_station['lon'], metro_station['lat']))
+# search through data set and look for closest latlong
+# calculate latlong distance:
+
+# example input
+# metro_stations = [
+#                     {'lat': 40.730328, 'lon': -73.992629, 'name': '8th St'},
+#                     {'lat': 40.741303, 'lon': -73.989344, 'name': '23rd St'},
+#                     {'lat': 40.735736, 'lon': -73.990568, 'name': 'Union Square'},
+#                 ]
+citibike_stations = [
+                        {'lat': 40.7423543, 'lon': -73.98915076, 'name': 'Broadway & W 24 St' },
+                        {'lat': 40.751551, 'lon': -73.993934, 'name': '8 Ave & W 33 St' },
+                        {'lat': 40.746745, 'lon': -74.007756, 'name': 'W 20 St & 11 Ave' },
+                    ]
+
+# example output
+# observation - top citibike station all next to the same metro stop
+# [
+#     {
+#         citibike station:  {'lat': 40.7423543, 'lon': -73.98915076, 'name': 'Broadway & W 24 St'},
+#         metro station:  {'lat': 40.741303, 'lon': -73.989344, 'name': 'Broadway,23rd St'},
+#         temp_distance 0.11795330311417092,
+#     }
+#
+#     {
+#         citibike station:  {'lat': 40.751551, 'lon': -73.993934, 'name': '8 Ave & W 33 St'},
+#         metro station:  {'lat': 40.741303, 'lon': -73.989344, 'name': 'Broadway,23rd St'},
+#         temp_distance 1.2025867415022102,
+#     }
+#
+#     {
+#         citibike station:  {'lat': 40.746745, 'lon': -74.007756, 'name': 'W 20 St & 11 Ave'},
+#         metro station:  {'lat': 40.741303, 'lon': -73.989344, 'name': 'Broadway,23rd St'},
+#         temp_distance 1.6639293851898207,
+#     }
+# ]
+
+# getting data from metro station csv file
+# Division,Line,Station_Name,Station_Latitude,Station_Longitude,Route_1,Route_2,Route_3,Route_4,Route_5,Route_6,Route_7,Route_8,Route_9,Route_10,Route_11,Entrance_Type,Entry,Exit_Only,Vending,Staffing,Staff_Hours,ADA,ADA_Notes,Free_Crossover,North_South_Street,East_West_Street,Corner,Latitude,Longitude
+metro_stations_csv = []
+with open('metro_station_entrances_nyc.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        # print(row['Station_Name'], row['Station_Latitude'], row['Station_Longitude'],)
+        metro_stations_csv.append({
+                                    'metro_station_name' : row['Station_Name'],
+                                    'lat' : float(row['Station_Latitude']),
+                                    'lon' : float(row['Station_Longitude']),
+                                })
+
+mode_distance = []
+def get_distance():
+    for citibike in citibike_stations:
+        distance_to_metro = 1000
+        shortest_metro = {}
+        for metro in metro_stations_csv:
+            temp_distance = manhattan_distance((metro['lat'], metro['lon']), (citibike['lat'], citibike['lon']))
+            # temp_distance = haversine(metro['lon'], metro['lat'], citibike['lon'], citibike['lat'])
+
+            if temp_distance < distance_to_metro:
+                distance_to_metro = temp_distance
+                shortest_metro = metro
+        mode_distance.append({'citibike_station': citibike, 'metro_station': shortest_metro, 'distance_to_metro': distance_to_metro})
+    return mode_distance
+
+get_distance()
+# print(get_distance())
